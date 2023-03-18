32db7d22a1ad8f17a67f88be0b840b52ecd5b95e
John Leeds <leedsjohn6@gmail.com>
2023-03-18 15:06:58 -0500

    Wrote first test

--- src/main.py
+++ src/main.py
@@ -1,7 +1,32 @@
-import hkociembasolver.solver as sv
+import random
+import cube
 
-cubestring = 'FFBLBRDLDUBRRFDDLRLUUUFB'
+def smartVrandom(n = 2):
+    rcube = cube.Cube()
+    rcube.scramble()
+    count = 1
+    while True:
+        solution = rcube.get_solution()
+        solution = solution.split()
+        if len(solution) < n:
+            solution = "".join(solution)
+        else:
+            solution = "".join(solution[:n])
+        rcube.move(solution)
+        if rcube.is_solved():
+            return count
 
-a = sv.solve(cubestring)
-print(a)
-quit()
+        count += 1
+        prev = ""
+        moves = "RUFLDB"
+        for i in range(n):
+            while True:
+                rotation = random.choice(moves)
+                if rotation != prev:
+                    break
+            prev = rotation
+            for _ in range(1, random.randrange(4)):
+                rcube.move(rotation)
+        if rcube.is_solved():
+            return count
+        count += 1
